d9bf86e073160b41f95fce8329f1d451a0d3c902
novodevz <78587100+novodevz@users.noreply.github.com>
2023-12-12 18:24:50 +0200

    fizzbuzz v1

---
+++ app.py
@@ -0,0 +1,8 @@
+from icecream import ic
+
+
+for n in range(1, 101):
+    if n % 15 == 0: print(ic('fizzbuzz'))
+    elif n % 3 == 0: print('fizz')
+    elif n % 5 == 0: print('buzz')
+    else: print(n)
